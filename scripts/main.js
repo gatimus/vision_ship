d33174ca281c65e87f84b67df7be00eb8f610205
William Lohan <william.g.lohan@gmail.com>
2015-10-25 11:14:01 -0700

    init

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+//var lodash = require(['lodash'], function(_) {});
+//var machina = require(['machina']);
+
+require(['machina'], function(machina){
+    return machina.Fsm.extend({ /* your stuff */});
+});
